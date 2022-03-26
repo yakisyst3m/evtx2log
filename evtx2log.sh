ec67d4173a45e7905acc50bb0d2c5add901c1d6a
yakisyst3m <98212776+yakisyst3m@users.noreply.github.com>
2022-03-26 14:05:19 +0100

    Update evtx2log.sh

--- evtx2log.sh
+++ evtx2log.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 #
-# GitHub : yakisyst3m 	25 jan 2022
+# GitHub : yakisyst3m 	
+# 2022_03_26 v1.11
 #
-if [[ $1 == '--help' ]] || [[ $# == 0 ]]
-then
+if [[ "$1" == '--help' ]] || [[ "$#" == 0 ]] ; then
 	echo -e "\nAide pour utiliser la commande :\n"
-        echo -e	"\t./evtx2Log.sh [chemin_evtx] [chemin_log_to_export]\n"
+        echo -e	"\t./evtx2Log.sh [dossier source chemin_evtx] [dossier destination chemin_log_to_export]\n"
 fi
 
 
 
-if [[ ! -d $1 ]]
-then
+if [[ ! -d "$1" ]] ; then
 	echo "Chemin source invalide"
-	elif [[ ! -d $2 ]]
+	elif [[ ! -d "$2" ]]
 	then
-		echo "Chemin destination invalide"
+		echo -e "Chemin destination invalide \n"
 fi
 
-if [[ $# -ge 1 ]] && [[ -d $1 ]] && [[ -d $2  ]]
-then
+if [[ "$#" -ge 1 ]] && [[ -d "$1" ]] && [[ -d "$2"  ]] ; then
 	# Renommage des logs
-	sudo chmod -R 750 $1
-	sudo chown -R $USER $1
-	rename 's/\%4|\ /_/g' $1/* 
+	sudo chmod -R 750 "$1"
+	sudo chown -R $USER "$1"
+	rename 's/\%4|\ /_/g' "$1"/* 
 
-	# Liste des fichier evtx
-	find $1 -name "*.evtx" -exec ls {} \; > /tmp/list
+	# Recherche de tous les fichiers .evtx de façon récursive du dossier source + faire une liste dans /tmp/list
+	find "$1" -name "*.evtx" -exec ls {} \; > /tmp/list
 
 	# Conversion evtx en log
 	for log in $(cat /tmp/list)
 		do
-		nomLog=$(basename $log)			      # Récup du nom de fichier
-		evtxexport -f text $1/$nomLog > $2/$nomLog.log 2>/dev/null      # conversion evtx vers .log
+		nomLog=$(basename "$log")			      # Récup du nom de fichier
+		evtxexport -f text "$1"/"$nomLog" > "$2"/"$nomLog".log 2>/dev/null      # conversion .evtx vers .log
 		done
 
 	rm /tmp/list
